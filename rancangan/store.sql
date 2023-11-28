f209ab66b6dbf0ce83860d44eecbc7051b715a5d
Glendy <138383520+Glendy1208@users.noreply.github.com>
2023-11-28 13:57:28 +0700

    menambahkan data pada sql (jgn dihapus produk)

--- rancangan/store.sql
+++ rancangan/store.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 26, 2023 at 04:50 AM
+-- Generation Time: Nov 28, 2023 at 07:56 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -55,16 +55,6 @@ CREATE TABLE `customers` (
   `usernamePelanggan` varchar(128) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `customers`
---
-
-INSERT INTO `customers` (`kodePelanggan`, `alamatPelanggan`, `passwordPelanggan`, `usernamePelanggan`) VALUES
-(1, 'Jl Setia Budi Surabaya', '123', 'glendy11'),
-(2, 'Gang Pop ice telang', '123', 'PPP'),
-(3, 'jl Tunjungan surabaya', '123', 'budi111'),
-(4, '12', 'a9f0e71fd97a882a27b3e2614026758a27de262ab3df7c18a01a7ff25e092ba5', 'g@gmail.com');
-
 -- --------------------------------------------------------
 
 --
@@ -97,13 +87,6 @@ CREATE TABLE `karyawan` (
   `passwordKaryawan` varchar(256) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `karyawan`
---
-
-INSERT INTO `karyawan` (`kodeKaryawan`, `kodeJabatan`, `usernameKaryawan`, `passwordKaryawan`) VALUES
-(1, 1, 'glendy', 'a9f0e71fd97a882a27b3e2614026758a27de262ab3df7c18a01a7ff25e092ba5');
-
 -- --------------------------------------------------------
 
 --
@@ -116,19 +99,6 @@ CREATE TABLE `orderdetail` (
   `subHarga` int(11) NOT NULL,
   `qty` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `orderdetail`
---
-
-INSERT INTO `orderdetail` (`kodeProduk`, `kodePesanan`, `subHarga`, `qty`) VALUES
-(21, 65, 444, 1),
-(21, 67, 2220, 5),
-(51, 65, 80000, 8),
-(51, 70, 70000, 7),
-(55, 64, 45000, 3),
-(55, 66, 75000, 5),
-(58, 71, 615, 5);
 
 -- --------------------------------------------------------
 
@@ -143,20 +113,6 @@ CREATE TABLE `orders` (
   `keterangan` varchar(16) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `orders`
---
-
-INSERT INTO `orders` (`kodePesanan`, `kodePelanggan`, `tanggalPesan`, `keterangan`) VALUES
-(64, 1, '2023-11-21 07:10:53', 'sudah'),
-(65, 1, '2023-11-22 11:38:13', 'sudah'),
-(66, 2, '2023-11-22 06:04:53', 'sudah'),
-(67, 2, '2023-11-22 06:05:03', 'belum'),
-(68, 3, '2023-11-22 06:05:37', 'belum'),
-(69, 1, '2023-11-22 12:25:01', 'sudah'),
-(70, 1, '2023-11-22 12:26:39', 'belum'),
-(71, 4, '2023-11-24 10:36:28', 'sudah');
-
 -- --------------------------------------------------------
 
 --
@@ -170,13 +126,6 @@ CREATE TABLE `pembayaran` (
   `total` int(11) NOT NULL,
   `metode` varchar(64) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `pembayaran`
---
-
-INSERT INTO `pembayaran` (`kodePembayaran`, `kodePesanan`, `waktuBayar`, `total`, `metode`) VALUES
-(1, 71, '2023-11-24 10:36:28', 615, 'COD');
 
 -- --------------------------------------------------------
 
@@ -200,13 +149,31 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`kodeProduk`, `kodeKategori`, `kodeSuplaier`, `namaProduk`, `gambarProduk`, `hargaProduk`, `stokProduk`, `deskripsiProduk`) VALUES
-(21, 1, 1, 'hahahahah', '6561c6c594ace.png', 1244555, 123, '100 gr'),
-(25, 1, 1, '&lt;h1&gt;hahaha&lt;/h1&gt;', 'tes.jpg', 10000000, 90, '&lt;h1&gt;hahaha&lt;/h1&gt;'),
-(51, 1, 2, 'oke', '6553827578dc9.png', 10000, 21297, ''),
-(55, 1, 9, 'coba', '655c2ef9d6280.', 15000, 0, ''),
-(56, 2, 11, 'asdad', '6560203d9e113.png', 123, 1, '100gr'),
-(57, 1, 1, '', '6560208fdbd2a.png', 0, 0, ''),
-(58, 1, 1, 'manusia', '6560210565212.png', 123, 217, 'sfsdfd');
+(71, 2, 18, 'Saikoro Wagyu Beef Cube', '65642600e95d2.jpeg', 160000, 119, '1 kg'),
+(72, 2, 18, 'Tenderloin Meltique Wagyu', '656426a09b9fc.jpeg', 169000, 80, '800 gr'),
+(73, 2, 18, 'Nishiawa Beef Yakiniku Cut', '656427722245a.jpeg', 500000, 132, '100 gr A5'),
+(74, 2, 19, 'Wagyu Beef Thin Slices', '656428dfc006a.jpeg', 149000, 99, '200 gr'),
+(75, 2, 19, 'Omi Beef', '6564298961c5b.jpeg', 600000, 155, '500 gram'),
+(76, 2, 19, 'Wagyu Bone Marrow', '65642a5794092.jpeg', 49000, 199, '2 pcs'),
+(77, 1, 16, 'Whole Chicken', '65642cecbfd65.jpeg', 59000, 180, '1 pcs'),
+(78, 1, 16, 'Fillet Chicken Breast', '65642e749e66f.jpeg', 109000, 100, '1 kg'),
+(79, 1, 16, 'Chicken Leg', '65642ee2085aa.jpeg', 69000, 188, '1 kg'),
+(80, 1, 16, 'Fillet Chicken Thigh', '65642f45d915d.jpeg', 99000, 245, '1 kg'),
+(81, 1, 16, 'Chicken Drumstick', '65642fd4d2397.jpeg', 54000, 112, '1 kg'),
+(82, 1, 16, 'Chicken Wings', '656430540c883.jpeg', 49000, 177, '1 kg'),
+(83, 1, 16, 'Chicken Neck', '656430f12b112.jpeg', 39000, 166, '1 kg'),
+(84, 3, 17, 'Rib Eye', '65645e2437a88.jpeg', 34000, 64, '200 gr'),
+(85, 3, 17, 'Chuck Eye', '65645e76b18c5.jpeg', 41000, 97, '200 gr'),
+(86, 3, 17, 'T Bone', '65645efff21fd.jpeg', 40000, 122, '200 gr'),
+(87, 3, 17, 'Round Cut', '65645f85ef738.jpeg', 31000, 134, '200 gr'),
+(88, 3, 17, 'Brisket Cut', '65645fed8d890.jpeg', 46000, 235, '200 gr'),
+(89, 3, 17, 'Tomahawk Cut', '656460c03e757.jpeg', 54000, 188, '400 gr'),
+(90, 4, 20, 'Forequarter Cut', '656462000f93a.jpeg', 59000, 177, '250 gr'),
+(91, 4, 20, 'Breast Flap', '6564631830ffc.jpeg', 54000, 43, '300 gr'),
+(92, 4, 20, 'Leg Chump', '656464980879e.jpeg', 36000, 43, '200 gr'),
+(93, 4, 20, 'Square Cut Shoulder', '6564651524f61.jpeg', 49000, 134, '300 gr'),
+(94, 4, 20, 'Short Loin Goat', '65646579290a0.jpeg', 69000, 376, '400 gr'),
+(95, 4, 20, 'Tenderloin Goat', '6564664359c7a.jpeg', 79000, 169, '300 gr');
 
 -- --------------------------------------------------------
 
@@ -226,11 +193,11 @@ CREATE TABLE `suplaier` (
 --
 
 INSERT INTO `suplaier` (`kodeSuplaier`, `namaSuplaier`, `telpSuplaier`, `alamatSuplaier`) VALUES
-(1, 'asasasasas', '099328432423', 'jakarta 5'),
-(2, 'CV Anubis aja', '0857483784', 'Jl Mawar'),
-(9, 'PT Rizky kanibal', '0895637284', 'simo jawar'),
-(11, 'Rizkyan Kanibal', '09866474', 'addsad'),
-(13, 'PT Rizky Karnivora', '0895377321', 'Jl Mawar VA no 120');
+(16, 'PT ABC Meat', '08123456789', 'Jl Raya No 123 Jakarta'),
+(17, 'CV MeatLand', '087798765432', 'Jl Gatot Subroto No 45 Surabaya'),
+(18, 'PT Sakura Import', '089654321098', 'Jl Importir No 7 Jakarta'),
+(19, 'PT Nippon Delights', '081543215678', 'Jl Nihon No 5 Surabaya'),
+(20, 'PT GoatKing', '08976543235', 'Jl Telang Indah Timur V no 11');
 
 -- --------------------------------------------------------
 
@@ -343,37 +310,37 @@ ALTER TABLE `jabatan`
 -- AUTO_INCREMENT for table `karyawan`
 --
 ALTER TABLE `karyawan`
-  MODIFY `kodeKaryawan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `kodeKaryawan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `kodePesanan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=72;
+  MODIFY `kodePesanan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=75;
 
 --
 -- AUTO_INCREMENT for table `pembayaran`
 --
 ALTER TABLE `pembayaran`
-  MODIFY `kodePembayaran` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `kodePembayaran` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `kodeProduk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=60;
+  MODIFY `kodeProduk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=96;
 
 --
 -- AUTO_INCREMENT for table `suplaier`
 --
 ALTER TABLE `suplaier`
-  MODIFY `kodeSuplaier` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `kodeSuplaier` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT for table `wallet`
 --
 ALTER TABLE `wallet`
-  MODIFY `kodeWallet` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `kodeWallet` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Constraints for dumped tables
